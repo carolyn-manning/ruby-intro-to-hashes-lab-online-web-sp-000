777379e931c954b5b5b25ac0adb45dba3670e40c
Carolyn Manning <carolyn.manning3@gmail.com>
2020-10-09 03:48:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,16 +27,11 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4,
       :names => {
-        :reading_railroad => {}
       }
     },
     :rent_in_dollars => {
-      :one_piece_owned => 25,
-      :two_pieces_owned => 50,
-      :three_pieces_owned => 100,
-      :four_pieces_owned => 200
+      :one_piece_owned => 25
       }
-    }
 end
 
 def monopoly_with_fourth_tier
