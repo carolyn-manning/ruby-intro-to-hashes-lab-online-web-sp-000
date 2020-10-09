80a74d43c46cdac6563b26a5c3f4582cddb26945
Carolyn Manning <carolyn.manning3@gmail.com>
2020-10-09 03:42:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,8 +29,8 @@ def monopoly_with_third_tier
         :reading_railroad => {},
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {}
-      },
-    }
+      }
+    },
     :rent_in_dollars => {
       :one_piece_owned => 25,
       :two_pieces_owned => 50,
@@ -38,7 +38,6 @@ def monopoly_with_third_tier
       :four_pieces_owned => 200
       }
     }
-  }
 end
 
 def monopoly_with_fourth_tier
